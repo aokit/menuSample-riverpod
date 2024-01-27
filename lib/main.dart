66b010f03a257124191642405a8ee5899f829581
aokit <aokit.aecl@gmail.com>
2024-01-27 22:05:30 +0900

    20240127-2204---

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,17 @@
 // で生成させた回答。いちおうこれで大丈夫なもの。
 // https://chat.openai.com/share/97fae998-430a-4db7-acf5-a0f039084278
 
+// ⬇
+// themeProvider　を　preferenceStateProvider　として値を　dark　に
+// 持たせる方法にした。　preferenceStateProvider　の実装の内容としては
+// PreferenceStateNotifier　の中に記載。構造は　PreferenceState　と
+// して定義してある。
+// 20240127-2204---
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final themeProvider = StateProvider<bool>((ref) => false);
+// final themeProvider = StateProvider<bool>((ref) => false);
 
 void main() {
   runApp(const ProviderScope(child: MyApp()));
@@ -31,10 +38,18 @@ class MenuScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(themeProvider); // StateController<bool> を取得する
+    // final isDarkMode = ref.watch(themeProvider); // StateController<bool> を取得する
 
     // テーマを取得
-    final theme = isDarkMode ? ThemeData.dark() : ThemeData.light();
+    // final theme = isDarkMode ? ThemeData.dark() : ThemeData.light();
+
+    //
+    final darkState = ref.watch(preferenceStateProvider.select((state) {
+      return state.dark;
+    }));
+    final preferenceNotifier = ref.read(preferenceStateProvider.notifier);
+    final theme = darkState ? ThemeData.dark() : ThemeData.light();
+    //
 
     return MaterialApp(
       theme: theme, // テーマを更新
@@ -58,10 +73,19 @@ class MenuScreen extends ConsumerWidget {
                 child: const Text('Go to Main Screen'),
               ),
               const SizedBox(height: 20),
-              Switch(
-                value: isDarkMode, // isDarkMode の状態を直接使用する
+              /* Switch(
+                // value: isDarkMode, // isDarkMode の状態を直接使用する
+                value: darkState,
                 onChanged: (value) {
-                  ref.read(themeProvider.notifier).state = value;
+                  // ref.read(themeProvider.notifier).state = value;
+                  preferenceNotifier.set(dark: value);
+                },
+              ), */
+              SwitchListTile(
+                title: const Text('Dark Mode'),
+                value: darkState,
+                onChanged: (value) {
+                  preferenceNotifier.set(dark: value);
                 },
               ),
             ],
@@ -77,10 +101,18 @@ class MainScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final isDarkMode = ref.watch(themeProvider); // StateController<bool> を取得する
+    // final isDarkMode = ref.watch(themeProvider); // StateController<bool> を取得する
 
     // テーマを取得
-    final theme = isDarkMode ? ThemeData.dark() : ThemeData.light();
+    // final theme = isDarkMode ? ThemeData.dark() : ThemeData.light();
+
+    //
+    final darkState = ref.watch(preferenceStateProvider.select((state) {
+      return state.dark;
+    }));
+    // final preferenceNotifier = ref.read(preferenceStateProvider.notifier);
+    final theme = darkState ? ThemeData.dark() : ThemeData.light();
+    //
 
     return MaterialApp(
       theme: theme, // テーマを更新
@@ -100,3 +132,63 @@ class MainScreen extends ConsumerWidget {
     );
   }
 }
+
+/* --- ================================================================================================= --- */
+
+@immutable
+class PreferenceState {
+  const PreferenceState({
+    this.dark = false,
+  });
+  final bool dark;
+  //
+  PreferenceState copyWith({
+    bool? dark,
+  }) {
+    return PreferenceState(
+      dark: dark ?? this.dark,
+    );
+  }
+}
+
+class PreferenceStateNotifier extends StateNotifier<PreferenceState> {
+  PreferenceStateNotifier() : super(const PreferenceState()) {
+    initialize();
+  }
+
+  Future initialize() async {
+    // final dark = //darkByHive;
+    // const dark = false;
+    set(
+      dark: false, //dark,
+    );
+  }
+
+  //
+  Future<bool> get _dark async {
+    return false;
+  }
+
+  //
+  Future<void> set({
+    bool? dark,
+  })
+  //
+  async {
+    state = state.copyWith(
+      dark: dark,
+    );
+    if (dark == null) {
+    } else {
+      // Hive.box<bool>(boolsBoxName).put('darkByHive', dark);
+      // bool? darkByHive = Hive.box<bool>(boolsBoxName).get('darkByHive');
+    }
+  }
+}
+
+final preferenceStateProvider =
+    StateNotifierProvider<PreferenceStateNotifier, PreferenceState>((ref) {
+  return PreferenceStateNotifier();
+});
+
+/* --- ================================================================================================= --- */
